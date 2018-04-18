5055d09f20a1d8c363d29e500899675aea613f14
mahdi <mahdisafari20002013@gmail.com>
2018-04-18 08:51:30 -0700

    edit view landing page and admin page to link nav bar

--- resources/views/webpage.blade.php
+++ resources/views/webpage.blade.php
@@ -10,7 +10,7 @@ URI     : https://colorlib.com
  -->
 
 <head>
-    <title>MobApp - App Landing Page Template</title>
+    <title>Job Hub</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -40,7 +40,7 @@ URI     : https://colorlib.com
         <div class="row">
             <div class="col-md-12">
                 <nav class="navbar navbar-dark navbar-expand-lg">
-                    <a class="navbar-brand" href="index.html"><img src="{{ asset('/style_land_page/images/logo.png') }}" class="img-fluid" alt="logo"></a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+                    <a class="navbar-brand" href="index.html"><img src="{{ asset('/style_land_page/images/jobsBook.png') }}" class="img-fluid" alt="logo"></a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
                     <div class="collapse navbar-collapse" id="navbar">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item"> <a class="nav-link active" href="#home">HOME <span class="sr-only">(current)</span></a> </li>
@@ -60,10 +60,10 @@ URI     : https://colorlib.com
 
 <header class="bg-gradient" id="home">
     <div class="container mt-5">
-        <h1>Mobile App Landing Page Template</h1>
-        <p class="tagline">The one and only solution for any kind of mobila app landing needs. Just change the screenshots and texts and you are good to go. </p>
+        <h1>Job Book</h1>
+        <p class="tagline">Job Book is the best web site for job announcment in Afghanistan for all people. </p>
     </div>
-    <div class="img-holder mt-3"><img src="{{ asset('/style_land_page/images/iphonex.png') }}" alt="phone" class="img-fluid"></div>
+    <div class="img-holder mt-3"><img src="{{ asset('/style_land_page/images/job_seeker_head.png') }}" alt="phone" class="img-fluid"></div>
 </header>
 
 <div class="client-logos my-5">
@@ -73,15 +73,12 @@ URI     : https://colorlib.com
 </div>
 
 <div class="section light-bg" id="features">
-    x
 
     <div class="container">
-
         <div class="section-title">
             <small>HIGHLIGHTS</small>
             <h3>Features you love</h3>
         </div>
-
 
         <div class="row">
             <div class="col-12 col-lg-4">
@@ -131,176 +128,17 @@ URI     : https://colorlib.com
 
 </div>
 <!-- // end .section -->
-<div class="section">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 offset-lg-6">
-                <div class="box-icon"><span class="ti-mobile gradient-fill ti-3x"></span></div>
-                <h2>Discover our App</h2>
-                <p class="mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vel exercitationem eveniet vero maxime ratione </p>
-                <a href="#" class="btn btn-primary">Read more</a>
-            </div>
-        </div>
-        <div class="perspective-phone">
-            <img src="{{ asset('/style_land_page/images/perspective.png') }}" alt="perspective phone" class="img-fluid">
-        </div>
-    </div>
-
-</div>
 <!-- // end .section -->
 
 
-<div class="section light-bg">
-    <div class="container">
-        <div class="section-title">
-            <small>FEATURES</small>
-            <h3>Do more with our app</h3>
-        </div>
 
-        <ul class="nav nav-tabs nav-justified" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link active" data-toggle="tab" href="#communication">Communication</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#schedule">Scheduling</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#messages">Messages</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#livechat">Live Chat</a>
-            </li>
-        </ul>
-        <div class="tab-content">
-            <div class="tab-pane fade show active" id="communication">
-                <div class="d-flex flex-column flex-lg-row">
-                    <img src="{{ asset('/style_land_page/images/graphic.png') }}" alt="graphic" class="img-fluid rounded align-self-start mr-lg-5 mb-5 mb-lg-0">
-                    <div>
-
-                        <h2>Communicate with ease</h2>
-                        <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium, nisi nisi fermentum enim, et sagittis dolor nulla vel sapien. Vestibulum sit amet mattis ante. Ut placerat dui eu nulla
-                            congue tincidunt ac a nibh. Mauris accumsan pulvinar lorem placerat volutpat. Praesent quis facilisis elit. Sed condimentum neque quis ex porttitor,
-                        </p>
-                        <p> malesuada faucibus augue aliquet. Sed elit est, eleifend sed dapibus a, semper a eros. Vestibulum blandit vulputate pharetra. Phasellus lobortis leo a nisl euismod, eu faucibus justo sollicitudin. Mauris consectetur, tortor
-                            sed tempor malesuada, sem nunc porta augue, in dictum arcu tortor id turpis. Proin aliquet vulputate aliquam.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="schedule">
-                <div class="d-flex flex-column flex-lg-row">
-                    <div>
-                        <h2>Scheduling when you want</h2>
-                        <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium, nisi nisi fermentum enim, et sagittis dolor nulla vel sapien. Vestibulum sit amet mattis ante. Ut placerat dui eu nulla
-                            congue tincidunt ac a nibh. Mauris accumsan pulvinar lorem placerat volutpat. Praesent quis facilisis elit. Sed condimentum neque quis ex porttitor,
-                        </p>
-                        <p> malesuada faucibus augue aliquet. Sed elit est, eleifend sed dapibus a, semper a eros. Vestibulum blandit vulputate pharetra. Phasellus lobortis leo a nisl euismod, eu faucibus justo sollicitudin. Mauris consectetur, tortor
-                            sed tempor malesuada, sem nunc porta augue, in dictum arcu tortor id turpis. Proin aliquet vulputate aliquam.
-                        </p>
-                    </div>
-                    <img src="{{ asset('/style_land_page/images/graphic.png') }}" alt="graphic" class="img-fluid rounded align-self-start mr-lg-5 mb-5 mb-lg-0">
-                </div>
-            </div>
-            <div class="tab-pane fade" id="messages">
-                <div class="d-flex flex-column flex-lg-row">
-                    <img src="{{ asset('/style_land_page/images/graphic.png') }}" alt="graphic" class="img-fluid rounded align-self-start mr-lg-5 mb-5 mb-lg-0">
-                    <div>
-                        <h2>Realtime Messaging service</h2>
-                        <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium, nisi nisi fermentum enim, et sagittis dolor nulla vel sapien. Vestibulum sit amet mattis ante. Ut placerat dui eu nulla
-                            congue tincidunt ac a nibh. Mauris accumsan pulvinar lorem placerat volutpat. Praesent quis facilisis elit. Sed condimentum neque quis ex porttitor,
-                        </p>
-                        <p> malesuada faucibus augue aliquet. Sed elit est, eleifend sed dapibus a, semper a eros. Vestibulum blandit vulputate pharetra. Phasellus lobortis leo a nisl euismod, eu faucibus justo sollicitudin. Mauris consectetur, tortor
-                            sed tempor malesuada, sem nunc porta augue, in dictum arcu tortor id turpis. Proin aliquet vulputate aliquam.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="livechat">
-                <div class="d-flex flex-column flex-lg-row">
-                    <div>
-                        <h2>Live chat when you needed</h2>
-                        <p class="lead">Uniquely underwhelm premium outsourcing with proactive leadership skills. </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium, nisi nisi fermentum enim, et sagittis dolor nulla vel sapien. Vestibulum sit amet mattis ante. Ut placerat dui eu nulla
-                            congue tincidunt ac a nibh. Mauris accumsan pulvinar lorem placerat volutpat. Praesent quis facilisis elit. Sed condimentum neque quis ex porttitor,
-                        </p>
-                        <p> malesuada faucibus augue aliquet. Sed elit est, eleifend sed dapibus a, semper a eros. Vestibulum blandit vulputate pharetra. Phasellus lobortis leo a nisl euismod, eu faucibus justo sollicitudin. Mauris consectetur, tortor
-                            sed tempor malesuada, sem nunc porta augue, in dictum arcu tortor id turpis. Proin aliquet vulputate aliquam.
-                        </p>
-                    </div>
-                    <img src="{{ asset('/style_land_page/images/graphic.png') }}" alt="graphic" class="img-fluid rounded align-self-start mr-lg-5 mb-5 mb-lg-0">
-                </div>
-            </div>
-        </div>
-
-
-    </div>
-</div>
 <!-- // end .section -->
 
-<div class="section">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <img src="{{ asset('/style_land_page/images/dualphone.png') }}" alt="dual phone" class="img-fluid">
-            </div>
-            <div class="col-md-6 d-flex align-items-center">
-                <div>
-                    <div class="box-icon"><span class="ti-rocket gradient-fill ti-3x"></span></div>
-                    <h2>Launch your App</h2>
-                    <p class="mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vel exercitationem eveniet vero maxime ratione </p>
-                    <a href="#" class="btn btn-primary">Read more</a></div>
-            </div>
-        </div>
-
-    </div>
-
-</div>
 <!-- // end .section -->
 
 
-<div class="section light-bg">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 d-flex align-items-center">
-                <ul class="list-unstyled ui-steps">
-                    <li class="media">
-                        <div class="circle-icon mr-4">1</div>
-                        <div class="media-body">
-                            <h5>Create an Account</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium obcaecati vel exercitationem </p>
-                        </div>
-                    </li>
-                    <li class="media my-4">
-                        <div class="circle-icon mr-4">2</div>
-                        <div class="media-body">
-                            <h5>Share with friends</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium obcaecati vel exercitationem eveniet</p>
-                        </div>
-                    </li>
-                    <li class="media">
-                        <div class="circle-icon mr-4">3</div>
-                        <div class="media-body">
-                            <h5>Enjoy your life</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum, urna eu pellentesque pretium obcaecati vel exercitationem </p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <img src="{{ asset('/style_land_page/images/iphonex.png') }}" alt="iphone" class="img-fluid">
-            </div>
-
-        </div>
-
-    </div>
-
-</div>
 <!-- // end .section -->
 
 <div class="section bg-gradient">
